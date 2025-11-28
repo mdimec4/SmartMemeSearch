4cacf643f0ee9e9b61972a4ea9411edb922ab343
Mihael Dimec <mihael.dimec@gmail.com>
2025-11-28 14:00:08 +0100

    more clear ui aroun search

---
+++ SmartMemeSearch.Wpf/Converters/TileWidthConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace SmartMemeSearch.Wpf.Converters
+{
+    public class TileWidthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double totalWidth)
+            {
+                double margin = 37;
+
+                return totalWidth - margin;
+            }
+
+            return 300; // safe fallback
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
